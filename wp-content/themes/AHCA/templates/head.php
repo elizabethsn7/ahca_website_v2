47e505fea0262d007843309e088c5b291e771390
Danny Arvizu <darvizu061@gmail.com>
2016-09-05 20:05:23 -0500

    Adding google fonts

--- wp-content/themes/AHCA/templates/head.php
+++ wp-content/themes/AHCA/templates/head.php
@@ -3,6 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href='https://fonts.googleapis.com/css?family=Allan:700' rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
   <?php wp_head(); ?>
   <script type="text/javascript">
     var templateDir = "<?php bloginfo('template_directory') ?>";
