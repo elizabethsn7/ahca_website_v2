ea06eca62ae5d20adaa39bc1eb5e1fd0e044b07a
Danny Arvizu <darvizu061@gmail.com>
2016-09-13 15:28:03 -0500

    Restyled awards font and color

--- wp-content/themes/AHCA/templates/head.php
+++ wp-content/themes/AHCA/templates/head.php
@@ -4,6 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href='https://fonts.googleapis.com/css?family=Allan:700' rel='stylesheet' type='text/css'>
   <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:600" rel="stylesheet">
   <?php wp_head(); ?>
   <script type="text/javascript">
     var templateDir = "<?php bloginfo('template_directory') ?>";
