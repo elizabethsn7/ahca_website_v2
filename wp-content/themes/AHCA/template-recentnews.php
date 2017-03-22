5bd8c78aac09dd3d16bd79059b07d442c8e3eafb
Liz Kipp <liz@elizabethkipp.com>
2017-03-22 23:58:09 +0000

    updated logo

---
+++ wp-content/themes/AHCA/template-recentnews.php
@@ -0,0 +1,50 @@
+<?php 
+/*
+  Template Name: Recent News Page
+  */
+?>
+<div id="page2">
+  <div class="container-fluid">
+    <h1>Recent News</h1>
+    
+      <div class="col-sm-8" style="background-color:lavenderblush;">testing</div>
+      <div class="col-sm-4" style="background-color:yellow;"><h4>Walter Szlanga</h4></div>
+      
+          <div class="row text-center" id="presidents">
+        <h3 class="title">OUR PRESIDENTS</h3>
+    
+        <div class="col-sm-2 president">
+            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
+            <h4>Walter Szlanga</h4>
+            <h5>1938 – 1947</h5>
+        </div>
+        <div class="col-sm-2 president">
+            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
+            <h4>George C. Stech</h4>
+            <h5>1948 – 1951</h5>
+        </div>
+        <div class="col-sm-2 president">
+            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
+            <h4>Norbert B. Chesta</h4>
+            <h5>1952 – 1955</h5>
+        </div>
+        <div class="col-sm-2 president">
+            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
+            <h4>Edward Ladewski</h4>
+            <h5>1956 – 1959</h5>
+        </div>
+        <div class="col-sm-2 president">
+            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
+            <h4>John T. Apke</h4>
+            <h5>1960 – 1961</h5>
+        </div>
+        <div class="col-sm-2 president">
+            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
+            <h4>Thad Bochenek</h4>
+            <h5>1962 – 1963</h5>
+        </div>
+    </div>
+  </div>
+</div>
+
+
