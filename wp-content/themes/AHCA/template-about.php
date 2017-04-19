6075fbfa1487d2b141d453bc681357e41da0dd5d
Elizabeth Kipp <liz@elizabethkipp.com>
2017-04-19 12:52:18 -0500

    added remaining content for History

--- wp-content/themes/AHCA/template-about.php
+++ wp-content/themes/AHCA/template-about.php
@@ -18,7 +18,55 @@
         <h4 class="text-center">Incorporated in 1938 to promote the general welfare of the residents of the Archer Heights Community</h4>
         <div class="row">
            <div class="col-sm-12">
-            <p>The Archer Heights Civic Association (AHCA) is an all volunteer, nonprofit, non-political group of homeowners, renters, businessmen and industry united by the common bond of keeping the neighborhood safe for all residents, protecting the property values of the community and addressing any major issues which affect the quality of life in our neighborhood.  As such, our civic association is very active in matters involving building code compliance, property zoning, commercial/business property redevelopment, beautification projects and crime prevention. To that end we work with the Aldermen of the 14th 22nd and 23rd Wards.  We also work with the 8th Police District and the Chicago Alternative Policing Strategy (CAPS) to publicize procedures and programs of crime prevention, homeowner, renter and business safety, and also personal safety.  Archer Heights is divided between Police Beats 821 and 815 and all of our members are urged to attend CAPS meetings to become familiar with the beat officers who actually patrol our community.</p>
+                <p>The Archer Heights Civic Association (AHCA) is an all volunteer, nonprofit, non-political group of homeowners, renters, businessmen and industry united by the common bond of keeping the neighborhood safe for all residents, protecting the property values of the community and addressing any major issues which affect the quality of life in our neighborhood.  As such, our civic association is very active in matters involving building code compliance, property zoning, commercial/business property redevelopment, beautification projects and crime prevention. To that end we work with the Aldermen of the 14th 22nd and 23rd Wards.  We also work with the 8th Police District and the Chicago Alternative Policing Strategy (CAPS) to publicize procedures and programs of crime prevention, homeowner, renter and business safety, and also personal safety.  Archer Heights is divided between Police Beats 821 and 815 and all of our members are urged to attend CAPS meetings to become familiar with the beat officers who actually patrol our community.</p>
+
+                <p>Our membership currently stands at 800+ addresses which consist of 750 families &amp single persons and 50 businesses/industry.  We are a very pro-business community group that works with our elected officials to welcome new businesses and industry into the community that will have a positive impact on the neighborhood. With the exception of July and August our civic association meets once each month on the second Wednesday for a general membership meeting at the UNO Veterans Memorial Campus School located at 47th &amp Kildare Avenue.  All meetings begin at 7:30 PM sharp and all community residents, renters/business owners are always welcome to attend.</p>
+
+                <p>Major accomplishments of the civic association are:
+                    <ul>
+                        <li>Involvement in building and zoning issues throughout the community to protect our neighborhood.</li>
+                        <li>Building of the firehouse on 47th Street at Komensky Ave.</li>
+                        <li>Building of Archer Park and the field house.</li>
+                        <li>Establishment of the Archer Branch Library and building of the new facility on Archer Avenue.</li>
+                        <li>Defeat of a proposed Greyhound Bus Station on Pulaski Rd.</li>
+                        <li>A 1 ½ year campaign to stop the building of a U.S. Postal processing plant at 47th &amp Kilbourn Ave.</li>
+                        <li>Worked with the city to relocate the Selfix Corporation (now Home Products Intl) to 47th &amp Kilbourn.</li>
+                        <li>Building of Curie High School.</li>
+                        <li>Voting two precincts dry to rid the community of nuisance taverns and liquor stores.</li>
+                        <li>Building of Catalpa Play lot in the 4300 block of Kedvale Ave.</li>
+                        <li>Heavy involvement in the petition process to get the Southwest Rapid Transit (Orange Line) built.</li>
+                        <li>Active participation in the route selection and design of the Orange Line which resulted in certain structural design elements being incorporated that mitigated noise impact on the residential areas.</li>
+                        <li>Worked with the Alderman to get the J.B. Hunt Facility established along 47th Street – Archer to Hamlin.</li>
+                        <li>Stopping a proposed waste transfer and recycling facility at 42nd Street, Knox to Keating Avenues.</li>
+                        <li>Stopping a proposed “Gentlemen’s Club” at 39th &amp Karlov.</li>
+                        <li>Worked with the Alderman to bring Aldi, Target, and Pete’s Fresh Market to Pulaski Rd.</li>
+                        <li>Worked with the Alderman and developer to bring the “Pulaski Promenade” shopping mall which brought Shoe Carnival, PetSmart, Marshalls, Ross Dress for Less to the 4100 – 4200 blocks of Pulaski Rd.</li>
+                        <li>Worked with the Alderman, developer and neighboring civic group to bring Walgreens to the northeast corner of Cicero &amp Archer Avenues.</li>
+                        <li>Worked with the City Department of Planning to address various traffic issues at main intersections.</li>
+                        <li>Stopped tattoo parlors and “adult book stores” from coming into the neighborhood.</li>
+                        <li>Demolition of abandoned American National Bank building at 53rd &amp Cicero.</li>
+                        <li>Demolition of derelict houses and garages.</li>
+                        <li>Installation of Speed Bumps in Crawford Industrial Area which eliminated weekend drag racing.</li>
+                    </ul>
+                </p>
+                <p>Some of our ongoing community activities are:
+                    <ul>
+                        <li>Funding and organizing an Easter Egg Hunt and Halloween Party in Archer Park for all community children.</li>
+                        <li>Awarding a Junior Citizens Award and a $100 tribute to a student of our five community grammar schools.</li>
+                        <li>Actively work with our local Boy Scout troops giving them the opportunity to earn Eagle Scout status.</li>
+                        <li>Sponsor of the Archer Manor Little League baseball teams.</li>
+                        <li>Actively working with the Chicago Police Department CAPS Program to combat gang crimes and other criminal activity.</li>
+                    </ul>
+                </p>
+                <p>AHCA Senior Officers also serve on the Midway Noise Compatibility Commission which has brought sound insulation to three grammar schools and thousands of homes in the community.  Several of our civic members are actively involved in the Police Department’s CAPS and Court Advocate Programs and the Police Commanders’ District Advisory Council.  We also have a volunteer Graffiti Report Team that weekly canvases the neighborhood to record and report any incidents of graffiti or tagging and follows with the appropriate city agency for expeditious cleanup.</p>
+
+                <p>Every other year we organize a House Beautiful Contest whereby owners of twenty-five exceptionally maintained/landscaped homes are recognized for their extra efforts. </p>
+
+                <p>In 2013 the Archer Heights Civic Association marked the 75th year of our founding and we have been advised that we are the oldest continuing operating volunteer civic association in the City of Chicago.</p>
+
+                <p>The community of Archer Heights is bounded on the west by Cicero Avenue; north - Sanitary & Ship Canal; east – BNSF RR tracks (east of Lawndale Avenue); and south - IHB RR tracks (south of the Orange Line).  As a result of the 2010 census ward redistricting Archer Heights is divided among three City of Chicago Wards - 14th, 22nd, 23rd and we actively meet and work with the three respective aldermen on numerous issues and projects.</p>
+
+                <p>If you live rent or work within the boundaries of our community we invite you join our civic association and encourage you to become active in our neighborhood group.  Please download an application from the “How to Become a Member” page and mail it with your dues to our office.  You will receive our monthly newsletter notifying you of happenings, issues, and events in and adjacent to our community, our next general membership meeting and featured guest speaker.</p>
             </div>
         </div>
 
