36d44d3472fcd06c47358a9cf4ad483b36a7959e
Elizabeth Kipp <liz@elizabethkipp.com>
2017-06-14 12:27:33 -0500

    added recent news page

--- wp-content/themes/AHCA/template-recentnews.php
+++ wp-content/themes/AHCA/template-recentnews.php
@@ -3,48 +3,32 @@
   Template Name: Recent News Page
   */
 ?>
-<div id="page2">
-  <div class="container-fluid">
-    <h1>Recent News</h1>
-    
-      <div class="col-sm-8" style="background-color:lavenderblush;">testing</div>
-      <div class="col-sm-4" style="background-color:yellow;"><h4>Walter Szlanga</h4></div>
-      
-          <div class="row text-center" id="presidents">
-        <h3 class="title">OUR PRESIDENTS</h3>
-    
-        <div class="col-sm-2 president">
-            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
-            <h4>Walter Szlanga</h4>
-            <h5>1938 – 1947</h5>
-        </div>
-        <div class="col-sm-2 president">
-            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
-            <h4>George C. Stech</h4>
-            <h5>1948 – 1951</h5>
-        </div>
-        <div class="col-sm-2 president">
-            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
-            <h4>Norbert B. Chesta</h4>
-            <h5>1952 – 1955</h5>
-        </div>
-        <div class="col-sm-2 president">
-            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
-            <h4>Edward Ladewski</h4>
-            <h5>1956 – 1959</h5>
-        </div>
-        <div class="col-sm-2 president">
-            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
-            <h4>John T. Apke</h4>
-            <h5>1960 – 1961</h5>
-        </div>
-        <div class="col-sm-2 president">
-            <img src="<?php bloginfo('template_directory');?>/dist/images/presidentIcon.png">
-            <h4>Thad Bochenek</h4>
-            <h5>1962 – 1963</h5>
-        </div>
+<div id="newsPage">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-3 topRow">
+        <h3>Important Events</h3>
+        <ul>
+          <li><a href="#">test</a></li>
+          <li><a href="#">test</a></li>
+          <li><a href="#">test</a></li>
+          <li><a href="#">test</a></li>
+        </ul>
+      </div>
+
+      <div class="col-sm-3">
+        <img src="<?php bloginfo('template_directory');?>/dist/images/new2.jpg" class="img-responsive">
+      </div>
+      <div class="col-sm-6 bg-primary">
+        <h1>Recent News</h1>
+        <p>The info here will not change.The info here will not change.The info here will not change.The info here will not change.The info here will not change.The info here will not change.The info here will not change.The info here will not change.</p>
+      </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-4 bg-primary secondRow"><h2>Lorem Ipsum</h2></div>
+        <div class="col-sm-4 bg-primary secondRow"><h2>Lorem Ipsum</h2></div>
+        <div class="col-sm-4 bg-primary secondRow"><h2>Lorem Ipsum</h2></div>
     </div>
   </div>
 </div>
-
-
