8a30adae484a0c8446f724c8b67c74cf1fb2f0b3
Elizabeth Kipp <liz@elizabethkipp.com>
2017-06-21 00:22:50 -0500

    adjusted layout of recent news page

--- wp-content/themes/AHCA/template-recentnews.php
+++ wp-content/themes/AHCA/template-recentnews.php
@@ -6,7 +6,7 @@
 <div id="newsPage">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-sm-3 impEvents hidden-xs">
+      <div class="col-xs-12 col-sm-2 impEvents hidden-xs">
         <h3>Important Events</h3>
         <ul>
           <li><a href="#">test</a></li>
@@ -16,47 +16,46 @@
         </ul>
       </div>
       
-        <div class="col-xs-6 col-sm-3 rnPic">
-          <img src="<?php bloginfo('template_directory');?>/dist/images/new2.jpg" class="img-responsive">
-        </div>
-        <div class="col-sm-6">
-          <h1>Recent News</h1>
-          <p>The info here will not change.The info here will not change.The info here will not change.The info here will not change.The info here will not change.The info here will not change.The info here will not change.The info here will not change.</p>
+        <div class="col-sm-4">
+        <img src="<?php bloginfo('template_directory');?>/dist/images/new2.jpg" class="img-responsive"></div>
+        <div class="col-sm-6"></div>
+          <h1 class="lead">Recent News</h1>
+          <p class="lead">The info here is your latest blog post. Once you add a new post it will be moved down to where the lorem ipsum is. The info here is your latest blog post. Once you add a new post it will be moved down to where the lorem ipsum is. The info here is your latest blog post. Once you add a new post it will be moved down to where the lorem ipsum is.</p>
         </div>
       
     </div>
 
     <div class="row">
 
-    <div class="col-sm-3 secondRow">
+    <div class="col-sm-4 secondRow">
 
   <div class="panel panel-default">
     <div class="panel-heading">
       <h3 class="panel-title">Lorem Ipsum</h3>
     </div>
     <div class="panel-body">
-      Panel content Panel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel content
+      This is your previous blog post. This is your previous blog post. This is your previous blog post. This is your previous blog post. This is your previous blog post. This is your previous blog post. 
     </div>
   </div>
   </div>
 
-<div class="col-sm-3 secondRow">
+<div class="col-sm-4 secondRow">
   <div class="panel panel-default">
     <div class="panel-heading">
       <h3 class="panel-title">Lorem Ipsum</h3>
     </div>
     <div class="panel-body">
-      Panel content Panel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel content
+      This is your previous blog post. This is your previous blog post. This is your previous blog post. This is your previous blog post. This is your previous blog post. This is your previous blog post.
     </div>
   </div>
   </div>
 
-<div class="col-sm-3 secondRow">
+<div class="col-sm-4 secondRow">
   <div class="panel panel-default">
     <div class="panel-heading">
       <h3 class="panel-title">Lorem Ipsum</h3>
     </div>
-    <div class="panel-body">Panel content Panel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel contentPanel content</div>
+    <div class="panel-body">This is your previous blog post. This is your previous blog post. This is your previous blog post. This is your previous blog post. This is your previous blog post. This is your previous blog post.</div>
   </div>
   </div>
     </div>
