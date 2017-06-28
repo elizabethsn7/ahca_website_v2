cfa5d2216c1a622e6d37831f600b8d101b44a952
Danny Arvizu <darvizu061@gmail.com>
2017-06-27 22:11:06 -0500

    Styled content-single.php for single blog posts

--- wp-content/themes/AHCA/templates/content-single.php
+++ wp-content/themes/AHCA/templates/content-single.php
@@ -1,15 +1,14 @@
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
-  </article>
+  <div class="container">
+    <article <?php post_class(); ?>>
+      <header>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?= the_post_thumbnail('medium', ['class' => 'img-responsive']);?>
+        <?php get_template_part('templates/entry-meta'); ?>
+      </header>
+      <div class="entry-content">
+        <?php the_content(); ?>
+      </div>
+    </article>
+  </div>
 <?php endwhile; ?>
